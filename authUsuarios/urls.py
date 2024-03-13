e6f3c0a29c88a4adf1545380b6a388b19744eeeb
drvv <drvv@hotmail.com>
2024-03-13 17:10:22 -0600

    Signin de usuarios.

--- authUsuarios/urls.py
+++ authUsuarios/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.welcome,name="authU_welcome"),
-    path("signup/", views.signup,name="authU_signup")
+    path("signup/", views.signup,name="authU_signup"),
+    path("logout/", views.signout,name="authU_signout"),
+    path("signin/", views.signin,name="authU_signin"),
 ]
--- authUsuarios/views.py
+++ authUsuarios/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login,logout,authenticate
 from django.db import IntegrityError
 #from django.http import HttpResponse
 
@@ -34,4 +34,26 @@ def signup(request):
             'mensaje':"La contraseña no coincide."
         })
         
-    
+def signout(request):
+    print("Entra al logout")
+    logout(request)
+    return redirect('authU_welcome')
+
+def signin(request):
+    if request.method == "GET":
+        return render(request, "signin.html",{
+            'form':AuthenticationForm
+        })
+    else:
+        print("Valida autenticación")
+        print(request.POST)
+        user= authenticate(request,username=request.POST['username'],password=request.POST['password'])
+        if user is None:
+            return render(request, "signin.html",{
+            'form':AuthenticationForm,
+            'mensaje':"username or password is incorrect."
+        })
+        else:
+            login(request,user)
+            return redirect('myapp_index')
+        
